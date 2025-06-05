51f07aed129f60388a8d656fd2c742180de098c2
Emmanuel Lopez <emlopezdev@gmail.com>
2025-06-05 14:30:22 -0400

    update to force re-run

--- js/main.mjs
+++ js/main.mjs
@@ -86,7 +86,6 @@ const showToast = () => {
 
     img.src = "./images/icon-success-check.svg";
     img.alt = "";
-
     span.innerText = `Message Sent!`;
 
     p.innerText = `Thanks for completing the form, ${formData.fname}. We'll be in touch soon via email: ${formData.email}!`;
