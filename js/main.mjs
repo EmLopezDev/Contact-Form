0ab1f80f4872e9ffa4f3775351f1d4f6cdb955dd
Emmanuel Lopez <emlopezdev@gmail.com>
2025-06-05 11:29:55 -0400

    added basic validation need to validate format of some inputs

--- js/main.mjs
+++ js/main.mjs
@@ -1,5 +1,5 @@
-const firstNameInput = document.getElementById("first-name");
-const lastNameInput = document.getElementById("last-name");
+const firstNameInput = document.getElementById("fname");
+const lastNameInput = document.getElementById("lname");
 const emailInput = document.getElementById("email");
 const queryTypeInput = document.querySelectorAll('input[name="query"]');
 const messageInput = document.getElementById("message");
@@ -9,21 +9,48 @@ const errorSpans = document.querySelectorAll(".error-message");
 const toast = document.getElementById("toast");
 
 const formData = {};
+let submitAttempt = false;
+
+const addErrors = (error) => {
+    const errorId = error.id.split("-")[0];
+    !formData[`${errorId}`]
+        ? error.classList.remove("hidden")
+        : error.classList.add("hidden");
+};
+
+const removeErrors = (error) => {
+    error.classList.add("hidden");
+};
+
+const validate = () => {
+    if (Object.keys(formData).length !== 6) {
+        errorSpans.forEach(addErrors);
+        return false;
+    } else {
+        errorSpans.forEach(removeErrors);
+        submitButton.removeAttribute("disabled");
+        return true;
+    }
+};
 
 firstNameInput.addEventListener("input", (e) => {
-    formData.firstName = e.target.value.trim();
+    formData.fname = e.target.value.trim();
+    if (submitAttempt) validate();
 });
 
 lastNameInput.addEventListener("input", (e) => {
-    formData.lastName = e.target.value.trim();
+    formData.lname = e.target.value.trim();
+    if (submitAttempt) validate();
 });
 
 emailInput.addEventListener("input", (e) => {
     formData.email = e.target.value.trim();
+    if (submitAttempt) validate();
 });
 
 const querySelected = (evt) => {
     formData.query = evt.target.value;
+    if (submitAttempt) validate();
 };
 
 queryTypeInput.forEach((input) => {
@@ -31,34 +58,27 @@ queryTypeInput.forEach((input) => {
 });
 
 messageInput.addEventListener("input", (e) => {
-    formData.message = e.target.value;
+    formData.message = e.target.value.trim();
+    if (submitAttempt) validate();
 });
 
 consentInput.addEventListener("input", (e) => {
-    formData.consent = e.target.checked;
+    formData.consent = e.target.checked ? e.target.checked.toString() : "";
+    if (submitAttempt) validate();
 });
 
 const showToast = () => {
     toast.style.top = "24px";
 };
 
-const canSubmit = () => {
-    if (Object.keys(formData).length === 0) {
-        errorSpans.forEach((error) => {
-            error.classList.remove("hidden");
-        });
-        return false;
-    } else if (Object.keys(formData).length !== 6) {
-        return false;
-    } else {
-        return true;
-    }
-};
-
 const submitForm = (evt) => {
     evt.preventDefault();
-    if (true) {
+    const canSubmit = validate();
+    submitAttempt = true;
+    if (canSubmit) {
         showToast();
+    } else {
+        submitButton.setAttribute("disabled", "true");
     }
 };
 
