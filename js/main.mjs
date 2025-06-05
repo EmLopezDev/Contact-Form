0cbdc87f43623b8a6fc5d4bb5f06d6448f5a894f
Emmanuel Lopez <emlopezdev@gmail.com>
2025-06-05 13:01:57 -0400

    updated validation logic

--- js/main.mjs
+++ js/main.mjs
@@ -27,24 +27,35 @@ const validate = () => {
         errorSpans.forEach(addErrors);
         return false;
     } else {
-        errorSpans.forEach(removeErrors);
         submitButton.removeAttribute("disabled");
+        errorSpans.forEach(removeErrors);
         return true;
     }
 };
 
+const nameCheck = (string) => {
+    return /^[a-zA-Z]+$/g.test(string);
+};
+
+const emailCheck = (string) => {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(string);
+};
+
 firstNameInput.addEventListener("input", (e) => {
-    formData.fname = e.target.value.trim();
+    const value = e.target.value.trim();
+    formData.fname = nameCheck(value) ? value : "";
     if (submitAttempt) validate();
 });
 
 lastNameInput.addEventListener("input", (e) => {
-    formData.lname = e.target.value.trim();
+    const value = e.target.value.trim();
+    formData.lname = nameCheck(value) ? value : "";
     if (submitAttempt) validate();
 });
 
 emailInput.addEventListener("input", (e) => {
-    formData.email = e.target.value.trim();
+    const value = e.target.value.trim();
+    formData.email = emailCheck(value) ? value : "";
     if (submitAttempt) validate();
 });
 
